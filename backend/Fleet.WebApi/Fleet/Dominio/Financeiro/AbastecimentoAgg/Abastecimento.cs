a603c045a4123e7a58dfb88894ca37c69265f7bc
Marcus <mvbios@gmail.com>
2015-03-07 01:37:23 -0300

    novas implementacoes

---
+++ backend/Fleet.WebApi/Fleet/Dominio/Financeiro/AbastecimentoAgg/Abastecimento.cs
@@ -0,0 +1,28 @@
+﻿using Kereta.Dominio.Frota.VeiculoAgg;
+using Kereta.Dominio.Refectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kereta.Dominio.Financeiro.AbastecimentoAgg
+{
+    public class Abastecimento : EntityBase
+    {
+        public Guid IdVeiculo { get; set; }
+
+        public virtual Veiculo Veiculo { get; set; }
+
+        public Guid IdViagem { get; set; }
+
+        public virtual Viagem Viagem { get; set; }
+
+        public DateTime Data { get; set; }
+        public virtual CombustivelDoVeiculo CombustivelDoVeiculo { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal Valor { get; set; }
+
+
+    }
+}
---
+++ backend/Fleet.WebApi/Fleet/Dominio/Financeiro/ViagemAgg/Viagem.cs
@@ -0,0 +1,23 @@
+﻿using Kereta.Dominio.Frota.VeiculoAgg;
+using Kereta.Dominio.Refectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kereta.Dominio.Financeiro.AbastecimentoAgg
+{
+    public class Viagem:EntityBase
+    {
+        public Guid IdVeiculo { get; set; }
+        public virtual Veiculo Veiculo { get; set; }
+        public DateTime? DataDeSaida { get; set; }
+        public DateTime? DataDeChegada { get; set; }
+        public decimal TempoPrevisto { get; set; }
+
+
+
+
+    }
+}
